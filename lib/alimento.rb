651a1e4e585406f1adb3f56b13b225378d702b03
Lisseth Vergaray Del Aguila <alu0100547160@ull.edu.es>
2017-10-18 01:51:10 +0000

    Método para calcular las áreas trapezoidales creado

--- lib/alimento.rb
+++ lib/alimento.rb
@@ -6,7 +6,17 @@ class Alimento
 		@g0 = g0
 		@gi = gi
 	end
-
+	
+	#Se calcula las 24 ares trapezoidales 
+	def areatrapezoide
+		$dt = 5
+		gi.each do |i|
+			$S = (((i-g0) + ((gi[i - 1]) - g0)) /2 ) * dt
+			Si.push(S)
+		end 
+		return Si
+	end
+	
 	def to_s
 		s = ""
 		s += "#{@titulo} "
